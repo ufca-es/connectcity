f1d3386470488799482f34a9614764f4ba805b2b
Dorian Dayvid Gomes Feitosa <dorian.dayvid@aluno.ufca.edu.br>
2025-09-13 00:06:49 +0000

    Adicionei os arquivos da interface

---
+++ interface/readme.md
@@ -0,0 +1,1 @@
+É só pra me ler mesmo, não sei o que tô fazendo não :)
---
+++ interface/streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title("Boas vindas ao ConnectCity!")
+st.write("Vamos fingir que eu sei fazer algo útil aqui...")
+st.write("Meu amigooooooooo funcionaaaaa")
