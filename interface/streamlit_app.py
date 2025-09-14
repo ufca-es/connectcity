5a5f3b8f9f503746adcf76432dedbcd8e58107fc
Dorian Dayvid Gomes Feitosa <dorian.dayvid@aluno.ufca.edu.br>
2025-09-14 14:49:52 +0000

    Adicionei uma barra de chat

--- interface/streamlit_app.py
+++ interface/streamlit_app.py
@@ -4,3 +4,5 @@ import streamlit as st
 st.title("Boas vindas ao ConnectCity!")
 st.write("Vamos fingir que eu sei fazer algo útil aqui...")
 st.write("Meu amigooooooooo funcionaaaaa")
+
+st.chat_input(placeholder="Insira sua mensagem aqui", key=None, max_chars=None, accept_file=False, file_type=None, disabled=False, on_submit=None, args=None, kwargs=None, width="stretch")
